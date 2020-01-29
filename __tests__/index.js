c479edddfbb861f30ec75a2f514f064bfb110b9e
cawfree <hello@cawfree.com>
2020-01-29 14:23:41 +0000

    Basic single level matcher implementation.

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,60 +2,73 @@ import '@babel/polyfill';
 
 import compose from '../src';
 
-const somethingThatHandlesAll = () => handle => {
-  handle('*', () => {
-    console.log('something that handles all');
-    return 42;
-  });
-};
+const addTwo = () => handle => handle(
+  '[Number]', (next, last) => {
+    console.log('last state was ',last);
+    return next.map(e => (e + 2));
+  },
+);
 
-const somethingThatHandlesAllMulti = () => handle => {
-  handle('handlesAllMulti', () => console.log('something that handles all'));
-  handle('handlesAllMultiRound2', () => console.log('something that handles all'));
-};
-
-const somethingThatHandlesTheFirstElement = () => handle => {
-  handle('handlesTheFirstElement', () => console.log('something that handles all'));
-  handle('handlesTheFirstElementRound2', () => console.log('something that handles all'));
-};
-
-const somethingThatHandlesTheSecondElement = () => handle => {
-  handle('handlesTheSecond', () => console.log('something that handles all'));
-  handle('handlesTheSecondRound2', () => console.log('something that handles all'));
-};
-
-const somethingThatHandlesTheFirstIndex = () => handle => {
-  handle('handlesTheFirstIndex', () => console.log('something that handles all'));
-};
-
-const somethingThatHandlesTheSecondIndex = () => handle => {
-  handle('handlesTheSecondIndex', () => console.log('something that handles all'));
-};
-
-const somethingThatHandlesTheSecondIndexFirstIndex = () => handle => {
-  handle('handlesTheSecondIndexFirstIndex', () => console.log('something that handles all'));
-  handle('handlesTheSecondIndexFirstIndexRound2', () => console.log('something that handles all'));
-};
+//const somethingThatHandlesAll = () => handle => {
+//  handle('*', () => {
+//    console.log('something that handles all');
+//    return 42;
+//  });
+//};
+//
+//const somethingThatHandlesAllMulti = () => handle => {
+//  handle('handlesAllMulti', () => console.log('something that handles all'));
+//  handle('handlesAllMultiRound2', () => console.log('something that handles all'));
+//};
+//
+//const somethingThatHandlesTheFirstElement = () => handle => {
+//  handle('handlesTheFirstElement', () => console.log('something that handles all'));
+//  handle('handlesTheFirstElementRound2', () => console.log('something that handles all'));
+//};
+//
+//const somethingThatHandlesTheSecondElement = () => handle => {
+//  handle('handlesTheSecond', () => console.log('something that handles all'));
+//  handle('handlesTheSecondRound2', () => console.log('something that handles all'));
+//};
+//
+//const somethingThatHandlesTheFirstIndex = () => handle => {
+//  handle('handlesTheFirstIndex', () => console.log('something that handles all'));
+//};
+//
+//const somethingThatHandlesTheSecondIndex = () => handle => {
+//  handle('handlesTheSecondIndex', () => console.log('something that handles all'));
+//};
+//
+//const somethingThatHandlesTheSecondIndexFirstIndex = () => handle => {
+//  handle('handlesTheSecondIndexFirstIndex', () => console.log('something that handles all'));
+//  handle('handlesTheSecondIndexFirstIndexRound2', () => console.log('something that handles all'));
+//};
 
 it('should export an argument filtering interface', async () => {
 
   const app = compose()
     // each handler deserves dedicated state, if it hasn't been executed, it cant get, it is entirely different data, too difficult, just share state
-    .use(somethingThatHandlesAll())
-    .use(somethingThatHandlesAllMulti())
+    //.use(somethingThatHandlesAll())
+    //.use(somethingThatHandlesAllMulti())
+    //.use(
+    //  // XXX: as a whole, without indexing
+    //  somethingThatHandlesTheFirstElement(), //give me the first element
+    //  somethingThatHandlesTheSecondElement(), // give me the second element
+    //)
     .use(
-      somethingThatHandlesTheFirstElement(),
-      somethingThatHandlesTheSecondElement(),
+      addTwo(),
+      //somethingThatHandlesAll(),
+      // XXX: this is first index of first element
+      //[somethingThatHandlesAll(), somethingThatHandlesAll()],
+      //[somethingThatHandlesAll()],
     )
-    .use(
-      [somethingThatHandlesTheFirstElement()],
-    )
-    .use(
-      [somethingThatHandlesTheFirstIndex(), somethingThatHandlesTheSecondIndex()],
-      [somethingThatHandlesTheFirstIndex(), [somethingThatHandlesTheSecondIndexFirstIndex()]],
-    );
+    //.use(
+    //  [somethingThatHandlesTheFirstIndex(), somethingThatHandlesTheSecondIndex()],
+    //  [somethingThatHandlesTheFirstIndex(), [somethingThatHandlesTheSecondIndexFirstIndex()]],
+    //);
 
-  await app('hi');
+  await app(2);
+  await app(4);
 
   expect(true).toBeTruthy();
 });
--- src/index.js
+++ src/index.js
@@ -1,20 +1,101 @@
 import { typeCheck } from 'type-check';
 
-const recurseUse = (e) => { 
+// XXX: Defines that we've encountered a handler; this is a layer which contains only handlers.
+// TODO: Need to enforce that we disallow empty handlers.
+const isArrayOfHandlers = e => Array.isArray(e) && e.length > 0 && (
+  e.reduce(
+    // TODO: Need to enforce function for typeCheck.
+    (r, f) => (!!r && typeCheck('{matches:String|Function,handler:Function,state:*}', f)),
+    true,
+  )
+);
+
+// So, it's the **contents of the child array** that defines the rules of propagation.
+const recurseUse = (e, parent=[]) => { 
   if (Array.isArray(e)) {
-    return e.reduce((arr, e) => [...arr, recurseUse(e)], []);
+    return e
+      .reduce(
+        (arr, f) => [...arr, recurseUse(f)],
+        [],
+      );
   }
+
   const handlers = [];
   const handle = (matches, handler) => handlers.push({ matches, handler, state: undefined });
   e(handle);
   if (handlers.length === 0) {
     throw new Error('A call to use() must define a minimum of a single handler.');
-  } else if (handlers.length === 1) {
-    const [handler] = handlers;
-    return handler;
   }
   return handlers;
 };
+
+const findHandlerByMatches = (data, [...handlers]) => handlers
+  .reduce(
+    (handler, current) => {
+      if (!handler) {
+        // TODO: Currently we only permit String based handlers.
+        if (typeCheck('String', current.matches)) {
+          if (typeCheck(current.matches, data)) {
+            return current;
+          }
+          throw new Error(`Could not find a valid matcher for ${data}.`);
+        }
+        // TODO: Later elevate to regexp.
+        throw new Error(`A matcher must be either a string or a function, encountered ${current.matches}.`);
+      }
+      return handler;
+    },
+    null,
+  );
+
+const freeze = state => (!!state && typeof state === 'object') ? Object.freeze(state) : state;
+
+const recurseApply = (data, stage) => Promise
+  .resolve()
+  .then(
+    () => {
+      // XXX: Special case: consume the entire argument without destructuring
+      //      if we're using a single array handler.
+      if (stage.length === 1 && isArrayOfHandlers(stage[0])) {
+        const [...handlers] = stage[0];
+        const handler = findHandlerByMatches(data, handlers);
+        if (handler) {
+          return Promise
+            .resolve()
+            // TODO: Should freeze result somehow
+            .then(() => handler.handler(data, handler.state))
+            .then(result => (handler.state = freeze(result)))
+            .then(
+              (e) => {
+                console.log('got result', e);
+              },
+            );
+        }
+      }
+      return Promise.reject(`A handler for ${data} could not be found.`);
+    },
+  );
+
+//// TODO: should write to stage
+//const recurseApply = (data, stage) => Promise
+//  .resolve()
+//  .then(
+//    () => {
+//      if (Array.isArray(stage)) {
+//        if (!isArrayOfHandlers(stage)) {
+//          //console.log(stage,'is not a handler', stage.length);
+//          stage.map(
+//            e => recurseApply(data, e),
+//          );
+//          return;
+//        } else {
+//          console.log('got one!', stage);
+//          return;
+//        }
+//      }
+//      throw new Error('Do not know how to handle!');
+//    },
+//  );
 
 export default () => {
   const mwr = [];
@@ -25,28 +106,28 @@ export default () => {
       .reduce(
         (p, stage, i) => p
           .then(
-            (dataFromLastStage) => {
-              if (Array.isArray(stage)) {
-                //// The data must match the given dimensions.
-                //if (dataFromLastStage.length >= stage.length) {
+            (dataFromLastStage) => recurseApply(dataFromLastStage, stage),
+              //if (Array.isArray(stage)) {
+              //  //// The data must match the given dimensions.
+              //  //if (dataFromLastStage.length >= stage.length) {
 
-                //}
-                //return Promise.reject(new Error(''));
-                //console.log(stage);
-                //throw 'do not know';
-              }
-              const { matches, handler, state } = stage;
-              if (typeCheck('String', matches)) {
-                if (typeCheck(matches, dataFromLastStage)) {
-                  return Promise
-                    .resolve()
-                    .then(() => handler(dataFromLastStage, state))
-                    .then(data => (stage.state = data))
-                }
-                return Promise.reject(new Error('No matches found for input data.'));
-              }
-              return Promise.reject(`Encountered unsupported handler. ${matches}`);
-            },
+              //  //}
+              //  //return Promise.reject(new Error(''));
+              //  //console.log(stage);
+              //  //throw 'do not know';
+              //}
+              //const { matches, handler, state } = stage;
+              //if (typeCheck('String', matches)) {
+              //  if (typeCheck(matches, dataFromLastStage)) {
+              //    return Promise
+              //      .resolve()
+              //      .then(() => handler(dataFromLastStage, state))
+              //      .then(data => (stage.state = data))
+              //  }
+              //  return Promise.reject(new Error('No matches found for input data.'));
+              //}
+              //return Promise.reject(`Encountered unsupported handler. ${matches}`);
+            //},
           ),
         Promise
           .resolve(input),
@@ -60,12 +141,13 @@ export default () => {
   r.use = (...args) => {
     if (args.length === 0) {
       throw new Error('A call to use() must specify at least a single handler.');
-    } else if (args.length === 1) {
-      const [arg] = args;
-      mwr.push(recurseUse(arg));
-    } else {
-      mwr.push(recurseUse(args));
     }
+    //} else if (args.length === 1) {
+    //  const [arg] = args;
+    //  mwr.push(recurseUse(arg));
+    //} else {
+      mwr.push(recurseUse(args));
+    //}
     return r;
   };
   return r;
