9b74c69f42b6dfe92a1deddf213172d8285205a6
cawfree <hello@cawfree.com>
2020-01-29 17:21:40 +0000

    Corrected handler response.

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # rippleware
-A middleware-inspired toolbox that promotes function extensibility.
+A middleware-inspired toolbox which enables you to design fully customizable functions.
+
+#### 🌠 tl;dr
+It's like a [Factory Pattern](https://www.dofactory.com/javascript/factory-method-design-pattern) for arbitrary sequences of data manipulation. You can think of it like [express](https://www.npmjs.com/package/express) for computation.
+
+## 🔥 Features
+  - Deeply configurable, user-extensible function definitions.
+    - Design endlessly configurable, arbitrarily long sequences of data manipulation.
+    - Define multiple routes based on [`type-check`](https://www.npmjs.com/package/type-check) rigorous declaration syntax.
+  - Optional persistent middleware state.
+    - You can cache and respond to middleware results from previous executions.
+  - Synchronous execution, independent of middleware asynchrony. (yes, _really_)
+    - If you're tired of `async`/`await` or `.then()`, you [don't have to use it at all](https://www.npmjs.com/package/deasync).
+  - A friendly interface. 👋
+    - If you know [middleware](https://expressjs.com/en/guide/writing-middleware.html), you know rippleware.
+    - Intuitive result indexing enables simple operation on deeply-nested propagated data.
+
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```bash
+npm install --save rippleware
+```
+
+Using [`yarn`]():
+
+```bash
+yarn add rippleware
+```
+
+##  Overview
+
+### 1. Hello, world!
+
+The only entity that is exported from ripple was `compose`, which we can `use()` to define each step in our function:
+
+```javascript
+import compose from 'rippleware';
+
+const app = compose()
+  .use(handle => handle('*', () => "Hello, world!")); // handle('*', () => Promise.resolve("Hello, world!")) would behave identically
+
+console.log(app()); // "Hello, world!"
+```
+
+Let's break this down. When make a call to use, we're expected to pass a `Function` that accepts a single argument, `handle`, which is used to define all of the routes for that step in your middleware based upon the input data shape. Here, we use the asterisk (`*`) to accept any data whatsoever. Therefore, when we make a call to `app()` with undefined arguments, rippleware routes the data successfully to the handler.
+
+It's important to note that your handler functions can operate _asynchronously_ via [`Promises`](https://developers.google.com/web/fundamentals/primers/promises) or [`async`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function)functions, the result will still return synchronously.
+
+===
+For a detailed look at the capabilities of routing strings, check out [type-check](https://www.npmjs.com/package/type-check).
+===
+
+**Note**:
+If you'd prefer, you can opt out of synchronous behaviour by passing `{ sync: false }` to your call to `compose()`.
+
+### 2. Routing
+
+You can make multiple calls to `handle` within a single middleware; these define the different operations that can be performed based upon the shape of the input data.
+
+```javascript
+import compose from 'rippleware';
+
+const app = compose()
+  .use(
+    (handle) => {
+      handle('[String]', () => "You passed a string!");
+      handle('*', () => "You didn't pass a string!");
+    },
+  );
+
+console.log(app('This is a string.')) // "You passed a string!"
+console.log({ life: 42 }) // "You didn't pass a string!"
+```
+
+## Contributing
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
--- src/index.js
+++ src/index.js
@@ -1,129 +1,131 @@
-import { typeCheck } from 'type-check';
+import { typeCheck } from "type-check";
 
 // XXX: Defines that we've encountered a handler; this is a layer which contains only handlers.
 // TODO: Need to enforce that we disallow empty handlers.
-const isArrayOfHandlers = e => Array.isArray(e) && e.length > 0 && (
+const isArrayOfHandlers = e =>
+  Array.isArray(e) &&
+  e.length > 0 &&
   e.reduce(
     // TODO: Need to enforce function for typeCheck.
-    (r, f) => (!!r && typeCheck('{matches:String|Function,handler:Function,state:*}', f)),
-    true,
-  )
-);
+    (r, f) =>
+      !!r && typeCheck("{matches:String|Function,handler:Function,state:*}", f),
+    true
+  );
 
 // So, it's the **contents of the child array** that defines the rules of propagation.
-const recurseUse = (e, parent=[]) => { 
+const recurseUse = (e, parent = []) => {
   if (Array.isArray(e)) {
-    return e
-      .reduce(
-        (arr, f) => [...arr, recurseUse(f)],
-        [],
-      );
+    return e.reduce((arr, f) => [...arr, recurseUse(f)], []);
   }
 
   const handlers = [];
-  const handle = (matches, handler) => handlers.push({ matches, handler, state: undefined });
+  const handle = (matches, handler) =>
+    handlers.push({ matches, handler, state: undefined });
   e(handle);
   if (handlers.length === 0) {
-    throw new Error('A call to use() must define a minimum of a single handler.');
+    throw new Error(
+      "A call to use() must define a minimum of a single handler."
+    );
   }
   return handlers;
 };
 
-const findHandlerByMatches = (data, [...handlers]) => handlers
-  .reduce(
-    (handler, current) => {
-      if (!handler) {
-        // TODO: Currently we only permit String based handlers.
-        if (typeCheck('String', current.matches)) {
-          if (typeCheck(current.matches, data)) {
-            return current;
-          }
-          throw new Error(`Could not find a valid matcher for ${data}.`);
+const findHandlerByMatches = (data, [...handlers]) =>
+  handlers.reduce((handler, current) => {
+    if (!handler) {
+      // TODO: Currently we only permit String based handlers.
+      if (typeCheck("String", current.matches)) {
+        if (typeCheck(current.matches, data)) {
+          return current;
         }
-        // TODO: Later elevate to regexp.
-        throw new Error(`A matcher must be either a string or a function, encountered ${current.matches}.`);
+        //throw new Error(`Could not find a valid matcher for ${data}.`);
       }
-      return handler;
-    },
-    null,
+      //// TODO: Later elevate to regexp.
+      //throw new Error(
+      //  `A matcher must be either a string or a function, encountered ${current.matches}.`
+      //);
+    }
+    return handler;
+  }, null);
+
+const freeze = state =>
+  !!state && typeof state === "object" ? Object.freeze(state) : state;
+
+const recurseApply = (data, stage) =>
+  Promise.resolve().then(() => {
+    if (!Array.isArray(stage) || stage.length === 0) {
+      return Promise.reject(
+        new Error("A call to use() must define at least a single handler.")
+      );
+    } else if (stage.length === 1 && isArrayOfHandlers(stage[0])) {
+      // XXX: Special case: consume the entire argument without destructuring
+      //      if we're using a single array handler.
+      const [...handlers] = stage[0];
+      const handler = findHandlerByMatches(data, handlers);
+      if (handler) {
+        return (
+          Promise.resolve()
+            // TODO: Should freeze result somehow
+            .then(() => handler.handler(data, handler.state))
+            .then(result => (handler.state = freeze(result)))
+        );
+      }
+      return Promise.reject(`Could not find a valid matcher for ${data}.`);
+    } else if (data.length >= stage.length) {
+      return Promise.all(
+        stage.map((s, i) => {
+          if (isArrayOfHandlers(s)) {
+            const datum = data[i];
+            const handler = findHandlerByMatches(datum, s);
+            if (handler) {
+              return Promise.resolve()
+                .then(() => handler.handler(datum, handler.state))
+                .then(result => (handler.state = freeze(result)));
+            }
+            return Promise.reject(
+              `Could not find a valid matcher for ${datum}.`
+            );
+          }
+          return recurseApply(data[i], s);
+        })
+      ).then(
+        // Otherwise, index the results.
+        results =>
+          stage.length > 1 && results.length > 1 ? results : results[0]
+      );
+    }
+    return Promise.reject(`A handler for ${data} could not be found.`);
+  });
+
+const executeMiddleware = (mwr, input) =>
+  mwr.reduce(
+    (p, stage, i) =>
+      p.then(dataFromLastStage => recurseApply(dataFromLastStage, stage)),
+    Promise.resolve(input)
   );
 
-const freeze = state => (!!state && typeof state === 'object') ? Object.freeze(state) : state;
-
-const recurseApply = (data, stage) => Promise
-  .resolve()
-  .then(
-    () => {
-      if (!Array.isArray(stage) || stage.length === 0) {
-        return Promise.reject(new Error('A call to use() must define at least a single handler.'));
-      }
-      else if (stage.length === 1 && isArrayOfHandlers(stage[0])) {
-        // XXX: Special case: consume the entire argument without destructuring
-        //      if we're using a single array handler.
-        const [...handlers] = stage[0];
-        const handler = findHandlerByMatches(data, handlers);
-        return Promise
-          .resolve()
-          // TODO: Should freeze result somehow
-          .then(() => handler.handler(data, handler.state))
-          .then(result => (handler.state = freeze(result)));
-      } else if (data.length >= stage.length) {
-        return Promise
-          .all(
-            stage.map(
-              (s, i) => {
-                if (isArrayOfHandlers(s)) {
-                  const datum = data[i];
-                  const handler = findHandlerByMatches(datum, s);
-                  return Promise
-                    .resolve()
-                    .then(
-                      () => handler.handler(datum, handler.state)
-                    )
-                    .then(result => (handler.state = freeze(result)));
-                }
-                return recurseApply(data[i], s);
-              },
-            ),
-          )
-          .then(
-            // Otherwise, index the results.
-            (results) => (stage.length > 1 && results.length > 1) ? results : results[0],
-          );
-      }
-      return Promise.reject(`A handler for ${data} could not be found.`);
-    },
-  );
-
-const executeMiddleware = (mwr, input) => mwr
-  .reduce(
-    (p, stage, i) => p
-      .then(dataFromLastStage => recurseApply(dataFromLastStage, stage)),
-    Promise
-      .resolve(input),
-  );
-
-export default (options = { sync: false }) => {
-  if (!typeCheck('{sync:Boolean}|Undefined', options)) {
-    throw new Error('Invalid options.');
+export default (options = { sync: true }) => {
+  if (!typeCheck("{sync:Boolean}|Undefined", options)) {
+    throw new Error("Invalid options.");
   }
   const mwr = [];
   const { sync } = options;
   function r(...input) {
     // TODO: Enforce this (check@test).
     r.use = () => {
-      throw new Error('It is not possible to make a call to use() after function execution.');
+      throw new Error(
+        "It is not possible to make a call to use() after function execution."
+      );
     };
     if (sync) {
-      const { loopWhile } = require('deasync');
+      const { loopWhile } = require("deasync");
       const result = { error: undefined, data: undefined, done: false };
 
-      new Promise(
-        resolve  => Promise
-          .resolve()
+      new Promise(resolve =>
+        Promise.resolve()
           .then(() => executeMiddleware(mwr, input))
           .then(data => resolve(Object.assign(result, { data, done: true })))
-          .catch(error => resolve(Object.assign(result, { error, done: true }))),
+          .catch(error => resolve(Object.assign(result, { error, done: true })))
       );
 
       loopWhile(() => !result.done);
@@ -138,10 +140,12 @@ export default (options = { sync: false }) => {
     }
     // XXX: Execute the input in stages.
     return executeMiddleware(mwr, input);
-  };
+  }
   r.use = (...args) => {
     if (args.length === 0) {
-      throw new Error('A call to use() must specify at least a single handler.');
+      throw new Error(
+        "A call to use() must specify at least a single handler."
+      );
     }
     mwr.push(recurseUse(args));
     return r;
